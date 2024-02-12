0d7d67a8c933e60d2007ab44f4d3f4fd59b680ba
WhirlyHat <156375713+WhirlyHat@users.noreply.github.com>
2024-02-12 12:00:47 -0500

    Update pwm-fan-control.py: Updated status message
    Added a fanState variable to track the fan's state of ON and OFF using a boolean value.
    The duty cycle will only change if both the threshold and fan state values are matched.
    Status message updated to show temperature AND fan status.

--- service/pwm-fan-control.py
+++ service/pwm-fan-control.py
@@ -13,6 +13,8 @@ PWM_FREQ = 100 # PWM frequency in Hertz
 PWM_MAX = 100 # PWM duty cycle MAXIMUM
 PWM_MIN = 0 # PWM duty cycle MINIMUM
 ################################################################
+fanState = 0 # Boolean value to track the fan's state (on/off)
+################################################################
 GPIO.setmode(GPIO.BCM) # Broadcom SOC channel names
 GPIO.setwarnings(False) # Suppress "RuntimeWarning: This channel is already in use"
 GPIO.setup(GPIO_PIN,GPIO.OUT) # Configure output channel
@@ -36,16 +38,26 @@ if __name__ == '__main__':
     try:
         while True:
             celsius = get_celsius()
-            print("Temperature is " + str(celsius) + "C")
-            # Turn PWM ON
-            if celsius > ON_THRESHOLD:
+            # Display temperature and fan state message depending on fan state
+            if fanState == 1:
+                print("Temperature: " + str(celsius) + "C ---- Fan State: ON (Active Cooling)")
+            else:
+                print("Temperature: " + str(celsius) + "C ---- Fan State: OFF (Passive Cooling)")
+
+            # Start fan if upper threshold reached AND fan is NOT already running.
+            if celsius > ON_THRESHOLD and fanState == 0:
                 pwm.ChangeDutyCycle(PWM_MAX)
-                print("Fan is on")
-            elif celsius < OFF_THRESHOLD:
+                print("UPPER threshold reached - fan switched -ON-")
+                fanState = 1
+            # Stop fan if lower threshold reached AND fan is running.
+            elif celsius < OFF_THRESHOLD and fanState == 1:
                 pwm.ChangeDutyCycle(PWM_MIN)
-                print("Fan is off")
+                print("LOWER threshold reached - fan switched -OFF-")
+                fanState = 0
+
             time.sleep(SLEEP_INTERVAL)
     except KeyboardInterrupt:
+        print("Stopping service...")
         pwm.stop()
+        print("Performing resource cleanup...")
         GPIO.cleanup() # Free up any resources used and return all channels back to safe defaults
-        print("Terminating service...")
