dfcdd984d441733ec1c650a9e870307cd5f7dd6d
WhirlyHat <156375713+WhirlyHat@users.noreply.github.com>
2024-02-12 10:14:22 -0500

    Uploading rewritten code
    I updated the installation and service scripts. I have not tested on the Raspberry Pi yet.

---
+++ deploy-service.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+################################################################
+SCRIPT_FULLPATH="$(realpath $0)"
+SCRIPT_DIR="$(dirname "${SCRIPT_FULLPATH}")"
+################################################################
+SVC_DIR="/etc/systemd/system/"
+SVC_BASE="pwm-fan-control"
+SVC_NAME="${SVC_BASE}.service"
+EXE_PATH="${SCRIPT_DIR}/service/${SVC_BASE}.py"
+################################################################
+
+# TEST VARIABLES
+# Commented out ; Used to troubleshoot
+#echo "CONSTANT / VARIABLE VALUES" 
+#echo "=========================="
+#echo "Script full path : $SCRIPT_FULLPATH"
+#echo "Script directory : $SCRIPT_DIR"
+#echo "Service directory: $SVC_DIR"
+#echo "Service base name: $SVC_BASE"
+#echo "Service name     : $SVC_NAME"
+#echo "Target executable: $EXE_PATH"
+#echo ""
+
+## Display custom error messages ##
+error-exit() {
+    echo "Error: $1"
+    exit 1
+}
+
+## Create the service unit file ##
+create-unit() {
+    # echo "create-unit function:" # Commented out ; Used to troubleshoot
+    echo "Creating a new service unit: ${SVC_DIR}${SVC_NAME}"
+    cat <<EOF 
+[Unit]
+Description=PWM Fan Control
+After=multi-user.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/python3 ${EXE_PATH}
+Restart=always
+KillSignal=SIGINT
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    # Verify that unit file is created
+    if [ -e ${SVC_DIR}${SVC_NAME} ]; then
+        echo "Successfully created service unit file."
+        return 0
+    else
+        error-exit "Failed to create service unit file."
+    fi
+}
+
+## Stop and remove the service ##
+delete-service() {
+    #echo "delete-service function:" # Commented out ; Used to troubleshoot
+    echo "Deleting the service: ${SVC_NAME}"
+    systemctl --quiet stop ${SVC_NAME} &> /dev/null || error-exit "Unable to stop the service."
+    systemctl --quiet disable ${SVC_NAME} &> /dev/null || error-exit "Unable to disable the service."
+}
+
+## Load and start the service ##
+add-service() {
+    #echo "add-service function:" # Commented out ; Used to troubleshoot
+
+    # Remove existing service
+    if systemctl is-active --quiet $SVC_NAME; then
+        delete-service
+    fi
+
+    # Create service unit file
+    create-unit
+
+    # Reload and reset systemd
+    echo "Reloading systemd daemon..."
+    systemctl --quiet daemon-reload &> /dev/null || error-exit "Unable to reload systemd."
+    
+    # Enable and start the service
+    echo "Enabling the service: ${SVC_NAME}"
+    systemctl --quiet enable ${SVC_NAME} &> /dev/null || error-exit "Unable to enable the service."
+    echo "Starting the service: ${SVC_NAME}"
+    systemctl --quiet start ${SVC_NAME} &> /dev/null || error-exit "Unable to start the service."
+}
+
+## Detect existing service installation ##
+if [ -e ${SVC_DIR}${SVC_NAME} ]; then
+    echo "Existing service installation found."
+    echo "Do you want to overwrite? (Y/n)"
+    read -r answer
+    if [ ${answer} = 'y' ] || [ ${answer} = 'Y' ]; then
+        add-service
+    else
+        exit 0
+    fi
+else
+    echo "Proceed with new service installation? (Y/n)"
+    read -r answer
+    if [ ${answer} = 'y' ] || [ ${answer} = 'Y' ]; then
+        add-service
+    else
+        exit 0
+    fi
+fi
+
+## Verify the service is active
+if systemctl is-active --quiet $SVC_NAME; then
+    echo "Service ${SVC_NAME} successfully enabled and started!"
+    exit 0
+else
+    error-exit "Failed to complete service deployment. Aborting."
+fi
---
+++ service/pwm-fan-control.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+import time
+import subprocess
+
+################################################################
+ON_THRESHOLD = 36 # Celsius degrees when fan turns ON
+OFF_THRESHOLD = 34 # Celsius degrees when fan turns OFF
+SLEEP_INTERVAL = 7 # Seconds between core temperature polling
+GPIO_PIN = 14 # GPIO pin used to control the fan
+PWM_FREQ = 100 # PWM frequency in Hertz
+PWM_MAX = 100 # PWM duty cycle MAXIMUM
+PWM_MIN = 0 # PWM duty cycle MINIMUM
+################################################################
+GPIO.setmode(GPIO.BCM) # Broadcom SOC channel names
+GPIO.setup(GPIO_PIN,GPIO.OUT) # Configure output channel
+pwm = GPIO.PWM(GPIO_PIN,PWM_FREQ) # Create PWM instance
+pwm.start(PWM_MIN) # Set initial PWM state
+
+def get_celsius():
+    celsius = subprocess.getoutput("vcgencmd measure_temp | sed 's/[^0-9.]//g'")
+    celsius = round(float(celsius))
+    try:
+        return celsius
+    except (IndexError, ValueError,) as e:
+        # exception types may be incorrect
+        raise RuntimeError('Could not obtain core temperature output.') from e
+
+if __name__ == '__main__':
+    # Validate the ON and OFF thresholds
+    if OFF_THRESHOLD >= ON_THRESHOLD:
+        raise RuntimeError('OFF_THRESHOLD must be less than ON_THRESHOLD')
+    
+    try:
+        while True:
+            celsius = get_celsius()
+            print("Temperature is " + str(celsius) + "C")
+            # Turn PWM ON
+            if celsius > ON_THRESHOLD:
+                pwm.ChangeDutyCycle(PWM_MAX)
+                print("Fan is on")
+            elif celsius < OFF_THRESHOLD:
+                pwm.ChangeDutyCycle(PWM_MIN)
+                print("Fan is off")
+            time.sleep(SLEEP_INTERVAL)
+    except KeyboardInterrupt:
+        pwm.stop()
+        GPIO.cleanup() # Free up any resources used and return all channels back to safe defaults
+        print("Terminating service...")
