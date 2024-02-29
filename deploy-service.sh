a95d8b157f5501568cf7d8c76422824b0fb8f4ed
WhirlyHat <156375713+WhirlyHat@users.noreply.github.com>
2024-02-29 16:51:37 -0500

    Update deploy-service.sh
    Script includes functions to perform tasks. Include error handling.

--- deploy-service.sh
+++ deploy-service.sh
@@ -8,29 +8,36 @@ SVC_DIR="/etc/systemd/system/"
 SVC_NAME="pwm-fan-control.service"
 EXE_PATH="${SCRIPT_DIR}/service/pwm-fan-control.py"
 ################################################################
+promptAnswer="c"
+################################################################
 
-# TEST VARIABLES
-# Commented out ; Used to troubleshoot
-#echo "CONSTANT / VARIABLE VALUES" 
-#echo "=========================="
-#echo "Script full path : $SCRIPT_FULLPATH"
-#echo "Script directory : $SCRIPT_DIR"
-#echo "Service directory: $SVC_DIR"
-#echo "Service name     : $SVC_NAME"
-#echo "Target executable: $EXE_PATH"
-#echo ""
-
-## Display custom error messages ##
-error-exit() {
-    echo "Error: $1"
+## Function: Display custom error messages ##
+show-error() {
+    echo "" ; echo "Error: $1" ; echo ""
     exit 1
 }
 
-## Create the service unit file ##
-create-unit() {
-    # echo "create-unit function:" # Commented out ; Used to troubleshoot
-    echo "Creating a new service unit: ${SVC_DIR}${SVC_NAME}"
-    cat <<EOF | sudo tee "${SVC_DIR}${SVC_NAME}" &> /dev/null 
+## Function: Prompt user for Y/N answer ##
+get-prompt() {
+    while true
+    do
+        read -N1 -t15 -p "$1 (y/N): "
+        if [ $? -gt 128 ]; then
+            printf "\nTimed out waiting for a user response.\n"
+            promptAnswer="c"
+            break
+        fi
+        case $REPLY in
+            [yY]) printf "\n\n" ; promptAnswer="${REPLY}" ; break ;;
+            [nN]) printf "\n\n" ; promptAnswer="${REPLY}" ; break ;;
+            *) printf "\nPlease type \'y\' or \'n\' to continue.\n" ;;
+        esac
+    done
+}
+
+## Function: Create the service unit file ##
+new-service() {
+    cat <<EOF | sudo tee "${SVC_DIR}${SVC_NAME}" &> /dev/null
 [Unit]
 Description=PWM Fan Control
 After=multi-user.target
@@ -45,73 +52,74 @@ KillSignal=SIGINT
 WantedBy=multi-user.target
 EOF
 
-    # Verify that unit file is created
-    if [ -e ${SVC_DIR}${SVC_NAME} ]; then
-        echo "Successfully created service unit file."
-        return 0
-    else
-        error-exit "Failed to create service unit file."
+    # Check if unit file was successfully created
+    if ! [ -f "${SVC_DIR}${SVC_NAME}" ]; then
+        show-error "Failed to create the service unit file."
     fi
 }
 
-## Stop and remove the service ##
-delete-service() {
-    #echo "delete-service function:" # Commented out ; Used to troubleshoot
-    echo "Deleting the service: ${SVC_NAME}"
-    systemctl --quiet stop ${SVC_NAME} &> /dev/null || error-exit "Unable to stop the service."
-    systemctl --quiet disable ${SVC_NAME} &> /dev/null || error-exit "Unable to disable the service."
-}
-
-## Load and start the service ##
-add-service() {
-    #echo "add-service function:" # Commented out ; Used to troubleshoot
-
-    # Remove existing service
-    if systemctl is-active --quiet $SVC_NAME; then
-        delete-service
+## Function: Stop and remove the service ##
+remove-service() {
+    printf "Removing the service..."
+    if systemctl is-active --quiet ${SVC_NAME}; then
+        systemctl --quiet stop ${SVC_NAME} &> /dev/null || show-error "Unable to stop the service."
+        systemctl --quiet disable ${SVC_NAME} &> /dev/null || show-error "Unable to disable the service."
     fi
 
-    # Create service unit file
-    create-unit
+    if [ -f "${SVC_DIR}${SVC_NAME}" ]; then
+        rm ${SVC_DIR}${SVC_NAME} &> /dev/null || show-error "Failed to remove service unit file."
+    fi
+    printf "DONE\n"
+}
 
-    # Set the python file to executable
-    sudo chmod +x ${EXE_PATH} &> /dev/null || error-exit "Failed to set file permissions: ${EXE_PATH}"
-
+## Function: Load and start the service ##
+set-service() {
     # Reload and reset systemd
-    echo "Reloading systemd daemon..."
-    systemctl --quiet daemon-reload &> /dev/null || error-exit "Unable to reload systemd."
-    
-    # Enable and start the service
-    echo "Enabling the service: ${SVC_NAME}"
-    systemctl --quiet enable ${SVC_NAME} &> /dev/null || error-exit "Unable to enable the service."
-    echo "Starting the service: ${SVC_NAME}"
-    systemctl --quiet start ${SVC_NAME} &> /dev/null || error-exit "Unable to start the service."
+    printf "Reloading systemd daemon: "
+    systemctl --quiet daemon-reload &> /dev/null || show-error "Unable to reload systemd."
+    printf "DONE\n"
+
+    # Enable the service
+    printf "Enabling service %s: " ${SVC_NAME}
+    systemctl --quiet enable ${SVC_NAME} &> /dev/null || show-error "Failed to enable the service."
+    printf "DONE\n"
+
+    # Start the service
+    printf "Starting service %s: " ${SVC_NAME}
+    systemctl --quiet start ${SVC_NAME} &> /dev/null || show-error "Unable to start the service."
+    printf "DONE\n"
 }
 
-## Detect existing service installation ##
-if [ -e ${SVC_DIR}${SVC_NAME} ]; then
-    echo "Existing service installation found."
-    echo "Do you want to overwrite? (Y/n)"
-    read -r answer
-    if [ ${answer} = 'y' ] || [ ${answer} = 'Y' ]; then
-        add-service
-    else
+# Detect previous service installation #
+printf "\nFinding previous %s installation: " ${SVC_NAME}
+if [ -f "${SVC_DIR}${SVC_NAME}" ]; then
+    # Prompt service overwrite
+    printf "FOUND\n"
+    get-prompt "Do you want to overwrite?"
+    if [ ${promptAnswer} != 'y' ] && [ ${promptAnswer} != 'Y' ]; then
         exit 0
     fi
+    remove-service
 else
-    echo "Proceed with new service installation? (Y/n)"
-    read -r answer
-    if [ ${answer} = 'y' ] || [ ${answer} = 'Y' ]; then
-        add-service
-    else
+    # Prompt service install
+    printf "NOT FOUND\n"
+    get-prompt "Proceed with new service installation?"
+    if [ ${promptAnswer} != 'y' ] && [ ${promptAnswer} != 'Y' ]; then
         exit 0
     fi
 fi
 
-## Verify the service is active
+# Set the python file to executable
+sudo chmod +x ${EXE_PATH} &> /dev/null || show-error "Failed to set file permissions: ${EXE_PATH}"
+
+# Install a new instance of service #
+printf "Creating a new service unit: " ; new-service ; printf "%s%s\n" ${SVC_DIR} ${SVC_NAME}
+printf "Loading and starting the service...\n" ; set-service
+
+# Verify the service is active
 if systemctl is-active --quiet $SVC_NAME; then
     echo "Service ${SVC_NAME} successfully enabled and started!"
     exit 0
 else
-    error-exit "Failed to complete service deployment. Aborting."
+    show-error "Failed to complete service deployment. Aborting."
 fi
